595f9b14a4021b3e4276f2bcb9236793e8c7318f
Akram <aminakram379@gmail.com>
2021-01-21 03:28:46 +0200

    .

--- train/models.py
+++ train/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class Poses(models.Model):
-    name = models.CharField(max_length=20)
+
+
+# class Courses(models.Model):
+#     name = models.CharField(max_length=50)
+#     description = models.TextField()
+
+
+# class Poses(models.Model):
+#     name = models.CharField(max_length=20)
     
 
-class Move(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField()
-    poses = models.ManyToManyField("train.Poses")
+# class Move(models.Model):
+#     name = models.CharField(max_length=50)
+#     description = models.TextField()
+#     poses = models.ManyToManyField("train.Poses")
